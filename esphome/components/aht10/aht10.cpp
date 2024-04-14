c0936b70de9e2c4e5472edcd61b099a489ba9190
Rami <rami.zayat9@gmail.com>
2024-04-14 19:48:53 +0200

    attempt fix 3

--- esphome/components/aht10/aht10.cpp
+++ esphome/components/aht10/aht10.cpp
@@ -21,21 +21,20 @@ namespace esphome {
 namespace aht10 {
 
 static const char *const TAG = "aht10";
-static const uint8_t AHTXX_INIT_CTRL_NOP = 0x00;         // safety margin, normally 3 attempts are enough: 3*30=90ms
+static const uint8_t AHTXX_INIT_CTRL_NOP = 0x00;  // safety margin, normally 3 attempts are enough: 3*30=90ms
 static const uint8_t AHT10_CALIBRATE_CMD[] = {0xE1};
 static const uint8_t AHT10_MEASURE_CMD[] = {0xAC, 0x33, AHTXX_INIT_CTRL_NOP};
-static const uint8_t AHT10_DEFAULT_DELAY = 5;    // ms, for calibration and temperature measurement
-static const uint8_t AHT10_HUMIDITY_DELAY = 30;  // ms
-static const uint8_t AHT10_ATTEMPTS = 3;         // safety margin, normally 3 attempts are enough: 3*30=90ms
-static const uint8_t AHTXX_SOFT_RESET_REG[] = {0xBA};         // safety margin, normally 3 attempts are enough: 3*30=90ms
+static const uint8_t AHT10_DEFAULT_DELAY = 5;          // ms, for calibration and temperature measurement
+static const uint8_t AHT10_HUMIDITY_DELAY = 30;        // ms
+static const uint8_t AHT10_ATTEMPTS = 3;               // safety margin, normally 3 attempts are enough: 3*30=90ms
+static const uint8_t AHTXX_SOFT_RESET_REG[] = {0xBA};  // safety margin, normally 3 attempts are enough: 3*30=90ms
 
-#define AHT1X_INIT_CTRL_NORMAL_MODE       0x00  //normal mode on/off       bit[6:5], for AHT1x only
-#define AHT1X_INIT_CTRL_CYCLE_MODE        0x20  //cycle mode on/off        bit[6:5], for AHT1x only
-#define AHT1X_INIT_CTRL_CMD_MODE          0x40  //command mode  on/off     bit[6:5], for AHT1x only
-#define AHTXX_INIT_CTRL_CAL_ON            0x08  //calibration coeff on/off bit[3]
+#define AHT1X_INIT_CTRL_NORMAL_MODE 0x00  // normal mode on/off       bit[6:5], for AHT1x only
+#define AHT1X_INIT_CTRL_CYCLE_MODE 0x20   // cycle mode on/off        bit[6:5], for AHT1x only
+#define AHT1X_INIT_CTRL_CMD_MODE 0x40     // command mode  on/off     bit[6:5], for AHT1x only
+#define AHTXX_INIT_CTRL_CAL_ON 0x08       // calibration coeff on/off bit[3]
 #define AHTXX_INIT (AHTXX_INIT_CTRL_CAL_ON | AHT1X_INIT_CTRL_NORMAL_MODE)
-static const uint8_t AHT2X_INIT_REG[] = {0xBE ,AHTXX_INIT ,AHTXX_INIT_CTRL_NOP };
-
+static const uint8_t AHT2X_INIT_REG[] = {0xBE, AHTXX_INIT, AHTXX_INIT_CTRL_NOP};
 
 void AHT10Component::setup() {
   ESP_LOGI(TAG, "Setting up AHT10...");
@@ -44,57 +43,56 @@ void AHT10Component::setup() {
 
   if (!this->write_bytes(0, AHTXX_SOFT_RESET_REG, sizeof(AHTXX_SOFT_RESET_REG))) {
     ESP_LOGE(TAG, "Communication with AHT10 AHTXX_SOFT_RESET_REG failed!");
-    //this->mark_failed();
-    //return;
+    // this->mark_failed();
+    // return;
   }
-
 
   if (!this->write_bytes(0, AHT2X_INIT_REG, sizeof(AHT2X_INIT_REG))) {
     ESP_LOGE(TAG, "Communication with AHT10 AHT2X_INIT_REG failed!");
-    //this->mark_failed();
-    //return;
+    // this->mark_failed();
+    // return;
   }
 
   if (!this->write_bytes(0, AHT10_CALIBRATE_CMD, sizeof(AHT10_CALIBRATE_CMD))) {
     ESP_LOGE(TAG, "Communication with AHT10 AHT10_CALIBRATE_CMD failed!");
-    //this->mark_failed();
-    //return;
+    // this->mark_failed();
+    // return;
   }
   uint8_t data = 0;
   if (this->write(&data, 1) != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "Communication with AHT10 write failed!");
-    //this->mark_failed();
-    //return;
+    // this->mark_failed();
+    // return;
   }
   delay(AHT10_DEFAULT_DELAY);
   if (this->read(&data, 1) != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "Communication with AHT10 read failed!");
-    //this->mark_failed();
-    //return;
+    // this->mark_failed();
+    // return;
   }
   if (this->read(&data, 1) != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "Communication with AHT10  read failed!");
-    //this->mark_failed();
-    //return;
+    // this->mark_failed();
+    // return;
   }
   if ((data & 0x68) != 0x08) {  // Bit[6:5] = 0b00, NORMAL mode and Bit[3] = 0b1, CALIBRATED
     ESP_LOGE(TAG, "AHT10 calibration failed!");
-   // this->mark_failed();
-   // return;
+    // this->mark_failed();
+    // return;
   }
   setupDone = true;
   ESP_LOGI(TAG, "AHT10 calibrated");
 }
 
 void AHT10Component::update() {
-  if (setupDone = false)
-  {
+  if (setupDone == false) {
     this->setup();
   }
 
   if (!this->write_bytes(0, AHT10_MEASURE_CMD, 3)) {
     ESP_LOGE(TAG, "Communication with AHT10 update write_bytes AHT10_MEASURE_CMD failed test!");
-    //this->status_set_warning();
+    // this->status_set_warning();
+    setupDone = false;
     return;
   }
   uint8_t data[6];
